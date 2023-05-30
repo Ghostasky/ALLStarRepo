8fdb771a8d34845ca399858c211cbe1652e0ff77
郁涛丶 <2992721672@qq.com>
2023-05-30 21:43:29 +0800

    Update a.py

--- a.py
+++ a.py
@@ -3,6 +3,7 @@ import requests
 import os
 
 key = os.environ.get("KEY")
+# key = "asdfsadfasdf"
 print(key)
 url = "https://api.github.com/user/starred"
 
@@ -12,51 +13,51 @@ headers = {
     "X-GitHub-Api-Version": "2022-11-28",
 }
 print(headers)
-# fp = open("./3.md", "w", encoding="utf-8")
-# fp.writelines("# ALL repo\n\n")
-# fp.writelines("这里显示我所有star的repo\n\n")
-# fp.writelines("| Num | Name |  auther   | Description  | Stars | Last update |\n")
-# fp.writelines("|-------|------|-------------|----------|-------|----|\n")
-# i = 1
-# while True:
-#     r = requests.get(url, headers=headers, params={"per_page": 1, "page": i})
-#     if r.status_code == 200:
-#         if len(r.content) != 2:
-#             a = r.json()
-#             print(i, a[0]["full_name"])
-#             print("\t", a[0]["description"])
-#             desc = a[0]["description"]
-#             if a[0]["description"] == None:
-#                 print("aaa")
-#                 desc = "None"
-#             elif len(str(a[0]["description"])) > 100:
-#                 desc = a[0]["description"][:100] + "..."
-#             fp.writelines(
-#                 "| "
-#                 + str(i)
-#                 + " |["
-#                 + a[0]["name"]
-#                 + "]("
-#                 + a[0]["html_url"]
-#                 + ") | ["
-#                 + a[0]["owner"]["login"]
-#                 + "]("
-#                 + a[0]["owner"]["html_url"]
-#                 + ") | "
-#                 + desc
-#                 + " | "
-#                 + str(a[0]["stargazers_count"])
-#                 + " | "
-#                 + a[0]["pushed_at"][:10]
-#                 + " |\n"
-#             )
-#         else:
-#             print("end")
-#             break
-#     else:
-#         fp.close()
-#         break
-#     i += 1
+fp = open("./3.md", "w", encoding="utf-8")
+fp.writelines("# ALL repo\n\n")
+fp.writelines("这里显示我所有star的repo\n\n")
+fp.writelines("| Num | Name |  auther   | Description  | Stars | Last update |\n")
+fp.writelines("|-------|------|-------------|----------|-------|----|\n")
+i = 1
+while True:
+    r = requests.get(url, headers=headers, params={"per_page": 1, "page": i})
+    if r.status_code == 200:
+        if len(r.content) != 2:
+            a = r.json()
+            print(i, a[0]["full_name"])
+            print("\t", a[0]["description"])
+            desc = a[0]["description"]
+            if a[0]["description"] == None:
+                print("aaa")
+                desc = "None"
+            elif len(str(a[0]["description"])) > 100:
+                desc = a[0]["description"][:100] + "..."
+            fp.writelines(
+                "| "
+                + str(i)
+                + " |["
+                + a[0]["name"]
+                + "]("
+                + a[0]["html_url"]
+                + ") | ["
+                + a[0]["owner"]["login"]
+                + "]("
+                + a[0]["owner"]["html_url"]
+                + ") | "
+                + desc
+                + " | "
+                + str(a[0]["stargazers_count"])
+                + " | "
+                + a[0]["pushed_at"][:10]
+                + " |\n"
+            )
+        else:
+            print("end")
+            break
+    else:
+        fp.close()
+        break
+    i += 1
 
 
-# print("fin")
+print("fin")
